2c89b6f76e0d672e8a9d05f0f6b5103dc634002c
Esteban Pintos <esteban.pintos@wetransfer.com>
2025-01-07 15:23:42 +0100

    Adds SurfTrip smart contract and unit tests

---
+++ src/SurfTrip.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.28;
+
+contract SurfTrip {
+
+    error SurfTrip_NotOrganizer();
+    error SurfTrip__DeadlineAlreadySet();
+    error SurfTrip__DepositIsTooLow(uint256 tripFee);
+    error SurfTrip__NotEnoughDeposits();
+    error SurfTrip__RefundFailed();
+    error SurfTrip__DeadlineMet();
+    error SurfTrip__WithdrawFailed();
+    
+    uint256 private immutable i_tripFee;
+    address private s_organizer;
+    uint256 private s_deadline;
+    bool private s_deadlineSet;
+    address[] private s_surfers;
+    mapping(address user => uint256) private s_surfersBalances;
+
+    event DepositMade(address indexed surfer, uint256 indexed amount, uint256 indexed newSurferBalance);
+    event DeadlineSet(uint256 indexed deadline);
+    event RefundMade(address indexed surfer, uint256 indexed amount, uint256 indexed surferBalanceLeft);
+    event WithdrawMade(address indexed host, uint256 indexed amountWithdraw);
+    event OrganizerChanged(address indexed previousOrganizer, address indexed newOrganizer);
+
+    constructor(uint256 tripFee) {
+        i_tripFee = tripFee;
+        s_organizer = msg.sender;
+    }
+
+    modifier onlyOrganizer() {
+        if (msg.sender != s_organizer) {
+            revert SurfTrip_NotOrganizer();
+        }
+        _;
+    }
+
+    modifier beforeDeadline() {
+        if (block.timestamp >= s_deadline) {
+            revert SurfTrip__DeadlineMet();
+        }
+        _;
+    }
+
+    function deposit() public payable {
+        if (msg.value < i_tripFee) {
+            revert SurfTrip__DepositIsTooLow(i_tripFee);
+        } 
+        if (s_surfersBalances[msg.sender] == 0) {
+            s_surfers.push(msg.sender);
+        }
+        s_surfersBalances[msg.sender] += msg.value;
+        emit DepositMade(msg.sender, msg.value, s_surfersBalances[msg.sender]);
+    }
+
+    function setDeadline(uint256 _days) external onlyOrganizer {
+        if (s_deadlineSet) {
+            revert SurfTrip__DeadlineAlreadySet();
+        } else {
+            s_deadline = block.timestamp + _days * 1 days;
+            s_deadlineSet = true;
+            emit DeadlineSet(s_deadline);
+        }
+    }
+
+    function refund(uint256 amount) external beforeDeadline {
+        uint256 surferBalance = s_surfersBalances[msg.sender];
+        if (surferBalance < amount) {
+            revert SurfTrip__NotEnoughDeposits();
+        }
+        (bool success,) = msg.sender.call{value: amount}("");
+        s_surfersBalances[msg.sender] -= amount;
+        if (!success) {
+            revert SurfTrip__RefundFailed();
+        }
+        emit RefundMade(msg.sender, amount, surferBalance - amount);
+    }
+
+    function withdraw() external onlyOrganizer {
+        for (uint256 index = 0; index < s_surfers.length; index++) {
+            address surfer = s_surfers[index];
+            s_surfersBalances[surfer] = 0;
+        }
+        s_deadlineSet = false;
+        delete s_surfers;
+
+        uint256 currentBalance = address(this).balance;
+        emit WithdrawMade(msg.sender, currentBalance);
+        (bool success,) = msg.sender.call{value: currentBalance}("");
+        if (!success) {
+            revert SurfTrip__WithdrawFailed();
+        }
+    }
+
+    function changeOrganizer(address newOrganizer) external onlyOrganizer {
+        address previousOrganizer = s_organizer;
+        s_organizer = newOrganizer;
+        emit OrganizerChanged(previousOrganizer, newOrganizer);
+    } 
+
+    receive() external payable {
+        deposit();
+    }
+
+    /* Getter functions */
+    function getTripFee() external view returns(uint256) {
+        return i_tripFee;
+    }
+
+    function getOrganizer() external view returns(address) {
+        return s_organizer;
+    }
+
+    function getDeadline() external view returns(uint256) {
+        return s_deadline;
+    }
+
+    function getSurferBalance(address surfer) external view returns(uint256) {
+        return s_surfersBalances[surfer];
+    }
+
+    function getSurfer(uint256 index) public view returns(address) {
+        return s_surfers[index];
+    }
+}
---
+++ test/unit/SurfTripTest.t.sol
@@ -0,0 +1,212 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.28;
+
+import {Test, console2} from "lib/forge-std/src/Test.sol";
+import {SurfTrip} from "src/SurfTrip.sol";
+import {Vm} from "forge-std/Vm.sol";
+
+contract SurfTripTest is Test {
+    SurfTrip public surfTrip;
+    uint256 public TRIP_FEE = 0.01 ether;
+    address public DEPLOYER = makeAddr("Deployer");
+    address public SURFER = makeAddr("Surfer");
+    uint256 public DEADLINE = 2;
+    uint256 public STARTING_TIMESTAP = 1;
+    uint256 public STARTING_SURFER_BALANCE = 10 ether;
+
+    event DepositMade(address indexed surfer, uint256 indexed amount, uint256 indexed newSurferBalance);
+    event DeadlineSet(uint256 indexed deadline);
+    event RefundMade(address indexed surfer, uint256 indexed amount, uint256 indexed surferBalanceLeft);
+    event WithdrawMade(address indexed host, uint256 indexed amountWithdraw);
+    event OrganizerChanged(address indexed previousOrganizer, address indexed newOrganizer);
+
+    function setUp() public {
+        vm.startPrank(DEPLOYER);
+        surfTrip = new SurfTrip(TRIP_FEE);
+        vm.warp(STARTING_TIMESTAP);
+        surfTrip.setDeadline(DEADLINE);
+        vm.stopPrank();
+    }
+
+    modifier surferContribution() {
+        vm.startPrank(SURFER);
+        vm.deal(SURFER, STARTING_SURFER_BALANCE);
+        surfTrip.deposit{ value: TRIP_FEE}();
+        vm.stopPrank();
+        _;
+    }
+
+    // GETTERS
+    function testGetTripFee() public view {
+        assertEq(surfTrip.getTripFee(), TRIP_FEE);
+    }
+
+    function testGetOrganizer() public view {
+        assertEq(surfTrip.getOrganizer(), DEPLOYER);
+    }
+
+    function testGetSurferBalance() public surferContribution {
+        assertEq(surfTrip.getSurferBalance(SURFER), TRIP_FEE);
+    }
+
+    function testGetDeadline() public view {
+        assertEq(surfTrip.getDeadline(), STARTING_TIMESTAP + DEADLINE * 1 days);
+    }
+
+    // SET DEADLINE
+    function testSetDeadlineBySomeoneElseFails() public {
+        vm.prank(SURFER);
+        vm.expectRevert(SurfTrip.SurfTrip_NotOrganizer.selector);
+        surfTrip.setDeadline(DEADLINE);
+    }
+
+    function testSetDeadlineEmitsEvent() public {
+        vm.startPrank(DEPLOYER);
+        surfTrip.withdraw(); // Resets s_deadlineSet
+        vm.expectEmit(true, false, false, false, address(surfTrip));
+        emit DeadlineSet(STARTING_TIMESTAP + DEADLINE * 1 days);
+        surfTrip.setDeadline(DEADLINE);
+        vm.stopPrank();
+    }
+
+    function testSetDeadlineWhenAlreadySetFails() public {
+        vm.prank(DEPLOYER);
+        vm.expectRevert(SurfTrip.SurfTrip__DeadlineAlreadySet.selector);
+        surfTrip.setDeadline(DEADLINE);
+    }
+
+    // DEPOSIT
+    function testDepositAddsContributorBalance() public {
+        vm.startPrank(SURFER);
+        vm.deal(SURFER, STARTING_SURFER_BALANCE);
+        surfTrip.deposit{ value: TRIP_FEE}();
+        assertEq(surfTrip.getSurferBalance(SURFER), TRIP_FEE);
+        surfTrip.deposit{ value: TRIP_FEE}();
+        vm.stopPrank();
+        assertEq(surfTrip.getSurferBalance(SURFER), TRIP_FEE * 2);
+        assertEq(address(surfTrip).balance, TRIP_FEE * 2);
+    }
+
+    function testDepositAddsSurferToArrayOfSurfers() public surferContribution {
+        assertEq(surfTrip.getSurfer(0), SURFER);
+    }
+
+    function testDepositFailsIfDepositIsNotEnough() public {
+        vm.prank(SURFER);
+        vm.deal(SURFER, STARTING_SURFER_BALANCE);
+        vm.expectRevert(abi.encodeWithSelector(SurfTrip.SurfTrip__DepositIsTooLow.selector, TRIP_FEE));
+        surfTrip.deposit{ value: TRIP_FEE - 1}();
+    }
+
+    function testDepositEmitsEvent() public {
+        vm.prank(SURFER);
+        vm.deal(SURFER, STARTING_SURFER_BALANCE);
+        vm.expectEmit(true, true, true, false, address(surfTrip));
+        emit DepositMade(SURFER, TRIP_FEE, TRIP_FEE);
+        surfTrip.deposit{ value: TRIP_FEE}();
+    }
+
+    // REFUND
+    function testRefundFailsIfDeadlineHasPassed() public {
+        vm.warp(STARTING_TIMESTAP + DEADLINE * 2 days);
+        vm.startPrank(SURFER);
+        vm.expectRevert(SurfTrip.SurfTrip__DeadlineMet.selector);
+        surfTrip.refund(TRIP_FEE);
+        vm.stopPrank();
+    }
+
+    function testRefundFailsIfNotEnoughtSurferBalance() public surferContribution {
+        vm.startPrank(SURFER);
+        vm.expectRevert(SurfTrip.SurfTrip__NotEnoughDeposits.selector);
+        surfTrip.refund(TRIP_FEE + 1);
+        vm.stopPrank();
+    }
+
+    function testRefundRefundsAmountToSurfer() public surferContribution {
+        uint256 startingBalance = address(SURFER).balance;
+        vm.startPrank(SURFER);
+        surfTrip.refund(TRIP_FEE);
+        vm.stopPrank();
+        uint256 newBalance = address(SURFER).balance;
+        assertEq(newBalance, startingBalance + TRIP_FEE);
+    }
+
+    function testRefundReducesSurferDepositsBalance() public surferContribution {
+        uint256 startingBalance = surfTrip.getSurferBalance(SURFER);
+        vm.startPrank(SURFER);
+        surfTrip.refund(TRIP_FEE);
+        vm.stopPrank();
+        uint256 newBalance = surfTrip.getSurferBalance(SURFER);
+        assertEq(newBalance, startingBalance - TRIP_FEE);
+    }
+
+    function testRefundEmitsEvent() public surferContribution {
+        vm.startPrank(SURFER);
+        vm.expectEmit(true, true, true, false, address(surfTrip));
+        emit RefundMade(SURFER, TRIP_FEE, 0);
+        surfTrip.refund(TRIP_FEE);
+        vm.stopPrank();
+    }
+
+    // WITHDRAW
+    function testWithdrawFailsIfNotOrganizer() public {
+        vm.startPrank(SURFER);
+        vm.expectRevert(SurfTrip.SurfTrip_NotOrganizer.selector);
+        surfTrip.withdraw();
+        vm.stopPrank();
+    }
+
+    function testWithdrawRemovesTheSurfersBalances() public surferContribution{
+        vm.prank(DEPLOYER);
+        surfTrip.withdraw();
+        assertEq(surfTrip.getSurferBalance(SURFER), 0);
+    }
+
+
+    function testWithdrawEmitsEvent() public {
+        vm.startPrank(DEPLOYER);
+        uint256 balance = address(surfTrip).balance;
+        vm.expectEmit(true, true, false, false, address(surfTrip));
+        emit WithdrawMade(DEPLOYER, balance);
+        surfTrip.withdraw();
+        vm.stopPrank();
+    }
+
+    function testWithdrawSendsMoneyToOrganizer() public {
+        uint256 startingBalance = address(surfTrip).balance;
+        vm.prank(DEPLOYER);
+        surfTrip.withdraw();
+        assertEq(address(surfTrip).balance, 0);
+        assertEq(address(DEPLOYER).balance, startingBalance);
+    }
+
+    // CHANGE ORGANIZER
+    function testChangeOrganizerFailsIfCalledBySomeoneElse() public {
+        vm.prank(SURFER);
+        vm.expectRevert(SurfTrip.SurfTrip_NotOrganizer.selector);
+        surfTrip.changeOrganizer(SURFER);
+    }
+
+    function testChangeOrganizerChangesOrganizer() public {
+        vm.prank(DEPLOYER);
+        surfTrip.changeOrganizer(SURFER);
+        assertEq(surfTrip.getOrganizer(), SURFER);
+    }
+
+    function testChangeOrganizerEmitsEvent() public {
+        vm.prank(DEPLOYER);
+        vm.expectEmit(true, true, false, false, address(surfTrip));
+        emit OrganizerChanged(DEPLOYER, SURFER);
+        surfTrip.changeOrganizer(SURFER);
+    }
+
+    // RECEIVE
+    function testReceiveAddsSurferBalance() public {
+        vm.prank(SURFER);
+        vm.deal(SURFER, STARTING_SURFER_BALANCE);
+        (bool success, ) = address(surfTrip).call{value: TRIP_FEE}("");
+        assertEq(success, true);
+        assertEq(surfTrip.getSurfer(0), SURFER);
+    }
+}
